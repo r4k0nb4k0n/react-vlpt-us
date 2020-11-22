541f3a999beaa3b108b7842c7feb2ac0a9f11bbd
r4k0nb4k0n <chj6097@gmail.com>
2020-11-22 22:00:14 +0900

    5. props의 기본 사용법

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
 import React from 'react';
 import Hello from './Hello';
-import './App.css';
 
 function App() {
-  const name = '너구리';
-  const style = {
-    backgroundColor: 'black',
-    color: 'cyan',
-    fontSize: 24,
-    padding: '1rem'
-  };
-  return (
-    <div>
-      <Hello />
-      <Hello />
-      <Hello />
-      <input />
-      <br />
-      <div style={ style }>{ name }</div>
-      <div className="gray-box"></div>
-    </div>
-  );
+	return (
+		<Hello name="react" />
+	);
 }
 
 export default App;
