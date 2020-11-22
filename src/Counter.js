afb73f8aff6ee1b3155b1553b6ea2cb07bf42f32
r4k0nb4k0n <chj6097@gmail.com>
2020-11-22 22:17:32 +0900

    7. 동적인 값 끼얹기, useState

---
+++ src/Counter.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+
+function Counter() {
+  const [counter, setCounter] = useState(0);
+  const onIncrease = () => {
+    console.log('+1')
+    setCounter(counter + 1)
+  }
+  const onDecrease = () => {
+    console.log('-1')
+    setCounter(counter - 1)
+  }
+  return (
+    <div>
+      <h1>{counter}</h1>
+      <button onClick={onIncrease}>+1</button>
+      <button onClick={onDecrease}>-1</button>
+    </div>
+  );
+}
+
+export default Counter;
