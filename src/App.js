9ffb7724358337c9a465caafc808ad0138957b0f
r4k0nb4k0n <chj6097@gmail.com>
2020-11-22 22:12:04 +0900

    6. 조건부 렌더링

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Wrapper from './Wrapper';
 function App() {
 	return (
 		<Wrapper>
-			<Hello name="react" color="red"/>
+			<Hello name="react" color="red" isSpecial/>
 			<Hello color="blue"/>
 		</Wrapper>
 	);
