e675ea8744ec64b06036932eb5c1473a5b0a66f2
r4k0nb4k0n <chj6097@gmail.com>
2020-11-24 12:29:55 +0000

    15. 배열에 항목 수정하기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,72 @@
-import React from 'react';
-import InputSample from './InputSample';
+import React, { useRef, useState } from 'react';
+import UserList from './UserList';
+import CreateUser from './CreateUser';
 
 function App() {
-	return (
-		<InputSample />
-	);
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: ''
+  });
+  const { username, email } = inputs;
+  const onChange = e => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value
+    });
+  };
+  const [users, setUsers] = useState([
+    {
+      id: 1,
+      username: '너구리',
+      email: 'public.neoguri@gmail.com',
+	  active: true
+    },
+    {
+      id: 2,
+      username: '라쿤',
+      email: 'raccoon@example.com',
+	  active: false
+    },
+    {
+      id: 3,
+      username: '오리너구리',
+      email: 'duckguri@example.com',
+	  active: false
+    }
+  ]);
+
+  const nextId = useRef(4);
+  const onCreate = () => {
+	const user = {
+      id: nextId.current,
+      username,
+      email
+    };
+    setUsers([...users, user]);
+    setInputs({
+      username: '',
+      email: ''
+    });
+    nextId.current += 1;
+  };
+  const onRemove = id => {
+    setUsers(users.filter(user => user.id !== id))
+  }
+  const onToggle = id => {
+    setUsers(users.map(user => user.id === id ? { ...user, active: !user.active } : user))
+  }
+  return (
+    <>
+      <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
+      <UserList users={users} onRemove={onRemove} onToggle={onToggle}/>
+    </>
+  );
 }
 
 export default App;
--- src/InputSample.js
+++ src/InputSample.js
@@ -1,35 +1,49 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 function InputSample() {
   const [inputs, setInputs] = useState({
     name: '',
     nickname: ''
-  })
+  });
+  const nameInput = useRef();
 
-  const { name, nickname } = inputs
+  const { name, nickname } = inputs; // 비구조화 할당을 통해 값 추출
 
-  const onChange = (e) => {
-    const { value, name } = e.target
+  const onChange = e => {
+    const { value, name } = e.target; // 우선 e.target 에서 name 과 value 를 추출
     setInputs({
-      ...inputs,
-      [name]: value
-    })
-  }
+      ...inputs, // 기존의 input 객체를 복사한 뒤
+      [name]: value // name 키를 가진 값을 value 로 설정
+    });
+  };
 
   const onReset = () => {
     setInputs({
       name: '',
       nickname: ''
-    })
-  }
+    });
+    nameInput.current.focus();
+  };
 
   return (
     <div>
-      <input onChange={onChange} value={name} name="name" placeholder="이름" />
-      <input onChange={onChange} value={nickname} name="nickname" placeholder="별명" />
+      <input
+        name="name"
+        placeholder="이름"
+        onChange={onChange}
+        value={name}
+        ref={nameInput}
+      />
+      <input
+        name="nickname"
+        placeholder="닉네임"
+        onChange={onChange}
+        value={nickname}
+      />
       <button onClick={onReset}>초기화</button>
       <div>
-        <b>값: {name} {nickname}</b>
+        <b>값: </b>
+        {name} ({nickname})
       </div>
     </div>
   );
