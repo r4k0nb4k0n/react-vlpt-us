28e83934aa85e6bb3ed80f475c32e88388aac1b9
r4k0nb4k0n <chj6097@gmail.com>
2020-12-08 20:22:15 +0900

    16. useEffect를 사용하여 마운트/언마운트/업데이트시 할 작업 설정하기

--- src/UserList.js
+++ src/UserList.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useEffect } from "react";
 
 function User({ user, onRemove, onToggle }) {
+  useEffect(() => {
+    console.log(user);
+  });
   return (
     <div>
-      <b style={{
-		  cursor: 'pointer',
-		  color: user.active ? 'green': 'black'
-		  }}
-		  onClick={() => onToggle(user.id)}
-		  >{user.username}</b> <span>({user.email})</span>
+      <b
+        style={{
+          cursor: "pointer",
+          color: user.active ? "green" : "black",
+        }}
+        onClick={() => onToggle(user.id)}
+      >
+        {user.username}
+      </b>
+      &nbsp;
+      <span>({user.email})</span>
       <button onClick={() => onRemove(user.id)}>삭제</button>
     </div>
   );
@@ -17,8 +25,13 @@ function User({ user, onRemove, onToggle }) {
 function UserList({ users, onRemove, onToggle }) {
   return (
     <div>
-      {users.map(user => (
-        <User user={user} key={user.id} onRemove={onRemove} onToggle={onToggle}/>
+      {users.map((user) => (
+        <User
+          user={user}
+          key={user.id}
+          onRemove={onRemove}
+          onToggle={onToggle}
+        />
       ))}
     </div>
   );
